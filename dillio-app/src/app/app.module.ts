4b750af7146aa0ec49c7fb1f27f46018dfb6a362
Osama <osama_allam@yamil.com>
2019-05-26 22:12:01 +0200

    create products area component

--- dillio-app/src/app/app.module.ts
+++ dillio-app/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { OwlModule } from 'ngx-owl-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SingleProductWrapComponent } from './shared/single-product-wrap/single-product-wrap.component';
+import { ProductAreaComponent } from './shared/product-area/product-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingleProductWrapComponent
+    SingleProductWrapComponent,
+    ProductAreaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
