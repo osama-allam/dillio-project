712185ff98518d1277bf9980bb159e4b81e2ba99
mariam <mariammohamed9596@gmail.com>
2019-05-26 23:49:47 +0200

    navbar edit

--- dillio-app/src/app/app.module.ts
+++ dillio-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropdownComponent } from './shared/dropdown/dropdown.component';
+import { NavComponent } from './core/nav/nav.component';
+import { HeaderComponent } from './core/header/header.component';
+import { TopHeaderComponent } from './core/header/top-header/top-header.component';
+import { MiddleHeaderComponent } from './core/header/middle-header/middle-header.component';
+import { BottomHeaderComponent } from './core/header/bottom-header/bottom-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent,
+    NavComponent,
+    HeaderComponent,
+    TopHeaderComponent,
+    MiddleHeaderComponent,
+    BottomHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ dillio-app/src/app/shared/dropdown/dropdown.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.css']
+})
+export class DropdownComponent implements OnInit {
+
+  isOpened: boolean;
+
+  constructor() { 
+    this.isOpened = false;
+  }
+
+  ngOnInit() {
+  }
+
+}
