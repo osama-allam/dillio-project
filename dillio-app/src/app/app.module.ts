08ac35b14fda21a2f653333b21746eec1b8ea782
Omar Ayyad <omarayyad36@gmail.com>
2019-05-26 15:05:11 +0200

    core header init

--- dillio-app/src/app/app.module.ts
+++ dillio-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { SideFilterComponent } from './features/side-filter/side-filter.component';
+import { FilterResultComponent } from './features/filter-result/filter-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideFilterComponent,
+    FilterResultComponent
   ],
   imports: [
     BrowserModule,
