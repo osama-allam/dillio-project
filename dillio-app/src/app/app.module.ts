1d922e72f63f6acca75ad47b6f56b7eedb5aff3b
Osama <osama_allam@yamil.com>
2019-05-26 15:39:32 +0200

    create product card and new assets
    in assets default.css style is added and images folder

--- dillio-app/src/app/app.module.ts
+++ dillio-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingleProductWrapComponent } from './shared/single-product-wrap/single-product-wrap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingleProductWrapComponent
   ],
   imports: [
     BrowserModule,
---
+++ dillio-app/src/app/shared/single-product-wrap/single-product-wrap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-product-wrap',
+  templateUrl: './single-product-wrap.component.html',
+  styleUrls: ['./single-product-wrap.component.css']
+})
+export class SingleProductWrapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
