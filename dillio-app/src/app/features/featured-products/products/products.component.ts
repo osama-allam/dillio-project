5de2507fa377a8b4058f0c433c90f3f201aefa71
mariam <mariammohamed9596@gmail.com>
2019-05-28 16:12:54 +0200

    fix feature products

--- dillio-app/src/app/features/featured-products/products/products.component.ts
+++ dillio-app/src/app/features/featured-products/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -10,6 +10,35 @@ export class ProductsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.productFeaturedActivator();
   }
 
+
+  productFeaturedActivator(): void {
+    $('.featured-product-active').owlCarousel({
+      loop: true,
+      nav: true,
+      autoplay: false,
+      autoplayTimeout: 5000,
+      navText: ['<i class="ion-ios-arrow-back"></i>', '<i class="ion-ios-arrow-forward"></i>'],
+      item: 3,
+      responsive: {
+         0: {
+             items: 1
+         },
+         768: {
+             items: 2
+         },
+         992: {
+             items: 2
+         },
+         1100: {
+             items: 2
+         },
+         1200: {
+             items: 2
+         }
+     }
+  })
+}
 }
