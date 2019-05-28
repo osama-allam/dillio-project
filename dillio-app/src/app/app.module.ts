1674cce3cd7afc2fcf772067188a9b348e110b8a
mariam <mariammohamed9596@gmail.com>
2019-05-28 15:36:13 +0200

    add featured products

--- dillio-app/src/app/app.module.ts
+++ dillio-app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NavComponent } from './core/nav/nav.component';
 import { HeaderComponent } from './core/header/header.component';
 import { MiddleHeaderComponent } from './core/header/middle-header/middle-header.component';
 import { BottomHeaderComponent } from './core/header/bottom-header/bottom-header.component';
+import { ProductsComponent } from './features/featured-products/products/products.component';
+import { ProductComponent } from './features/featured-products/product/product.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { BottomHeaderComponent } from './core/header/bottom-header/bottom-header
     NavComponent,
     HeaderComponent,
     MiddleHeaderComponent,
-    BottomHeaderComponent
+    BottomHeaderComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
