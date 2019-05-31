f39df216def40ea97e9dd342a322def40ae72369
mariam <mariammohamed9596@gmail.com>
2019-05-31 23:33:31 +0200

    fix aboutus counter

--- dillio-app/src/app/features/aboutus/counter/counter.component.ts
+++ dillio-app/src/app/features/aboutus/counter/counter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
@@ -10,6 +12,32 @@ export class CounterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.productFeaturedActivator();
+  }
+
+
+  productFeaturedActivator(): void {
+    $('.counter').each(function() {
+      var $this = $(this),
+          countTo = $this.attr('data-count');
+      
+      $({ countNum: $this.text()}).animate({
+        countNum: countTo
+      }, 
+    
+      {
+    
+        duration: 8000,
+        easing:'linear',
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+        }
+    
+      });  
+    });
   }
 
 }
