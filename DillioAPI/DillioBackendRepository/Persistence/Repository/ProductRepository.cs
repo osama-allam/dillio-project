3c1a4581c7d44f7ecbcef5417c699842da209fac
Omar Ayyad <omarayyad36@gmail.com>
2019-06-16 17:22:25 +0200

    commit changes

--- DillioAPI/DillioBackendRepository/Persistence/Repository/ProductRepository.cs
+++ DillioAPI/DillioBackendRepository/Persistence/Repository/ProductRepository.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Text;
+using DAL_Dillio_Project.Core.Domain;
+using DAL_Dillio_Project.Core.Repositories;
 
 namespace DAL_Dillio_Project.Persistence.Repository
 {
-    public class ProductRepository
+    public class ProductRepository:Repository<Product>,IProductRepository
     {
+        public ProductRepository(DbContext context) : base(context)
+        {
+        }
+
+        //add mycontext
     }
 }
