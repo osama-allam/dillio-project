06d304330e41d977b406b3a9a147104e212b3438
Tarek Hesham <tarekheaham1995@gmail.com>
2019-06-16 19:20:19 +0200

    m

---
+++ DillioAPI/DillioRepository/Persistence/Repository/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL_Dillio_Project.Core.Domain;
+using DAL_Dillio_Project.Core.Repositories;
+using System.Data.Entity;
+
+namespace DAL_Dillio_Project.Persistence.Repository
+{
+    public class ProductRepository:Repository<Product>,IProductRepository
+    {
+        public ProductRepository(DbContext context) : base(context)
+        {
+        }
+
+        //add mycontext
+    }
+}
