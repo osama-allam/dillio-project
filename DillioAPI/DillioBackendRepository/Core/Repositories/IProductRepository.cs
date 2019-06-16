4a8f9a3092166b25120e20419476f21c970a8248
Tarek Hesham <tarekheaham1995@gmail.com>
2019-06-16 15:06:41 +0200

    add IRepository and edit IProductRepository

--- DillioAPI/DillioBackendRepository/Core/Repositories/IProductRepository.cs
+++ DillioAPI/DillioBackendRepository/Core/Repositories/IProductRepository.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DAL_Dillio_Project.Core.Domain;
 
 namespace DAL_Dillio_Project.Core.Repositories
 {
-    public class IProductRepository
+    public interface IProductRepository:IRepository<Product>
     {
     }
 }
