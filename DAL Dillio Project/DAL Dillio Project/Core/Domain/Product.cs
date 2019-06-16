e50a25e6d7828d20a9c39828aae20ae84636d349
Omar Ayyad <omarayyad36@gmail.com>
2019-06-16 10:36:40 +0200

    adding product and image domains

--- DAL Dillio Project/DAL Dillio Project/Core/Domain/Product.cs
+++ DAL Dillio Project/DAL Dillio Project/Core/Domain/Product.cs
@@ -6,5 +6,9 @@ namespace DAL_Dillio_Project.Core.Domain
 {
     public class Product
     {
+        public int Id { get; set; }
+        public float Price { get; set; }
+        public float Discount { get; set; }
+        public ICollection<Image> Images { get; set; }
     }
 }
