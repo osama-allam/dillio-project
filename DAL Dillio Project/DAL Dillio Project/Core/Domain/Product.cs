94d77a66317bc49de84df4e84a2b92759ad22196
OmarAyyad <omarayyad36@gmail.com>
2019-06-16 10:26:10 +0200

    commiting backend

---
+++ DAL Dillio Project/DAL Dillio Project/Core/Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL_Dillio_Project.Core.Domain
+{
+    public class Product
+    {
+    }
+}
---
+++ DAL Dillio Project/DAL Dillio Project/Persistence/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL_Dillio_Project.Persistence.EntityConfigurations
+{
+    public class ProductConfiguration
+    {
+    }
+}
