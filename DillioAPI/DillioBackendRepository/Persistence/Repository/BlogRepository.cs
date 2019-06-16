f81cfbbf3361b7b32cf2d668ecd87eaab22feee6
Tarek Hesham <tarekheaham1995@gmail.com>
2019-06-16 17:14:56 +0200

    commiting repositories

---
+++ DillioAPI/DillioBackendRepository/Persistence/Repository/BlogRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using DAL_Dillio_Project.Core.Repositories;
+using DillioBackendRepository.Core.Domain;
+
+namespace DAL_Dillio_Project.Persistence.Repository
+{
+    public class BlogRepository:Repository<Blog>,IBlogRepository
+    {
+        public BlogRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ DillioAPI/DillioBackendRepository/Persistence/Repository/CommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using DAL_Dillio_Project.Core.Repositories;
+using DillioBackendRepository.Core.Domain;
+
+namespace DAL_Dillio_Project.Persistence.Repository
+{
+    public class CommentRepository:Repository<Comment>,ICommentRepository
+    {
+        public CommentRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
