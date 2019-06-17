13c03f8347d82de1282d5752cc9789e423cebd84
Omar Ayyad <omarayyad36@gmail.com>
2019-06-17 11:39:45 +0200

    commiting omar back

--- DillioApi/DillioRepository/Core/Repositories/ICommentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DillioBackendRepository.Core.Domain;
-
-namespace DAL_Dillio_Project.Core.Repositories
-{
-    public interface ICommentRepository:IRepository<Comment>
-    {
-        
-    }
-}
