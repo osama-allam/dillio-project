50fa2d2c48ddc3296760d6fea6a1116530c011b3
Omar Ayyad <omarayyad36@gmail.com>
2019-06-17 12:50:05 +0200

    adding entities in unitofwork along with order and review repos

--- DillioAPI/DillioRepository/Persistence/UnitOfWork.cs
+++ DillioAPI/DillioRepository/Persistence/UnitOfWork.cs
@@ -2,14 +2,30 @@
 using DAL_Dillio_Project.Core.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Text;
+using DAL_Dillio_Project.Persistence.Repository;
+using DillioRepository.Core.Repositories;
+using DillioRepository.Persistence.Repository;
 
 namespace DAL_Dillio_Project.Persistence
 {
     public class UnitOfWork : IUnitOfWork
     {
-        public UnitOfWork()
+        private DillioContext _context;
+        public UnitOfWork(DillioContext _context)
         {
+            this._context = _context;
+
+            Blogs = new BlogRepository(_context);
+            Categories = new CategoryRepository(_context);
+            Comments = new CommentRepository(_context);
+            Images = new ImageRepository(_context);
+            Likes = new LikeRepository(_context);
+            Products = new ProductRepository(_context);
+            Stores = new StoreRepository(_context);
+            Reviews = new ReviewRepository(_context);
+            Orders = new OrderRepository(_context);
 
         }
 
@@ -27,9 +43,20 @@ namespace DAL_Dillio_Project.Persistence
 
         public IStoreRepository Stores { get; }
 
+        public IReviewRepository Reviews { get; }
+        
+        public IOrderRepository Orders { get; }
+
+        public IReviewRepository ReviewRepository => throw new NotImplementedException();
+
         public int Complete()
         {
-            throw new NotImplementedException();
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
         }
     }
 }
