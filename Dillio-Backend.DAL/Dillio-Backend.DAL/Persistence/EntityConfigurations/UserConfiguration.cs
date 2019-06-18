0ce766983242cf95e63abfd9b17870bf997a841c
Tarek Hesham <tarekheaham1995@gmail.com>
2019-06-19 00:15:28 +0200

    add relations of user with entities and editing it's configuration

---
+++ Dillio-Backend.DAL/Dillio-Backend.DAL/Persistence/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Dillio_Backend.BLL.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dillio_Backend.DAL.Persistence.EntityConfigurations
+{
+    public class UserConfiguration:IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+
+            builder.Property(u => u.FirstName)
+                .HasColumnType("nvarchar")
+                .HasMaxLength(250);
+
+            builder.Property(u => u.LastName)
+                .HasColumnType("nvarchar")
+                .HasMaxLength(250);
+
+            builder.HasMany(u => u.Blogs)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.FK_UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(u => u.Reviews)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.FK_UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(u => u.Comments)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.FK_UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(u => u.Likes)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.FK_UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(u => u.Orders)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.FK_UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
