b360834c2bd1788e5cef21a998303e03e9577fac
Ahmadaboelfath <ahmadaboelfath@gmail.com>
2019-06-18 14:59:30 +0200

    Initial backend model

---
+++ Dillio-Backend.DAL/Dillio-Backend.BLL/Core/Domain/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dillio_Backend.BLL.Core.Domain
+{
+    /// <summary>
+    /// This Class is responisble for adding any additional fields in the user
+    /// table
+    /// </summary>
+  public class ApplicationUser : IdentityUser
+    {
+        
+
+    }
+}
---
+++ Dillio-Backend.DAL/Dillio-Backend.DAL/Persistence/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dillio_Backend.BLL.Core;
+using Dillio_Backend.BLL.Core.Domain;
+using Dillio_Backend.BLL.Core.Repositories;
+using Dillio_Backend.DAL.Persistence.Repository;
+
+namespace Dillio_Backend.DAL.Persistence
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        #region ReadOnlyMemberVariables
+
+        private readonly ApplicationDbContext _context;
+
+        #endregion
+
+        #region Repositories Member Variables
+
+        public IApplicationUserRepository ApplicationUser { get; set; }
+
+
+        #endregion
+
+        #region Constructor
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            ApplicationUser = new ApplicationUserRepository(context);
+
+        }
+
+        #endregion
+
+        #region Methods
+        public int Complete()
+        {
+          return  _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        #endregion
+
+    }
+}
