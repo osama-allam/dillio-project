810d0b7de495ad25e3220c9b80897879a8c97d62
Tarek Hesham <tarekheaham1995@gmail.com>
2019-06-18 17:09:35 +0200

    trying to add refrences and identity classes

---
+++ DillioAPI/DillioCore/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL_Dillio_Project.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DillioCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        public UnitOfWork UnitOfWork;
+       
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult  Get()
+        {
+            
+            string[] s={"ss","dd"};
+            return Ok(s);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DillioAPI/DillioCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DillioCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
