d4ebdbe1a739e226f23892c42745900f075b3006
Tarek Hesham <tarekheaham1995@gmail.com>
2019-06-20 00:23:20 +0200

    add product view model

--- Dillio-Backend.DAL/Dillio-Backend.API/ViewModel/ProductViewModel.cs
+++ Dillio-Backend.DAL/Dillio-Backend.API/ViewModel/ProductViewModel.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dillio_Backend.BLL.Core.Domain;
 
 namespace Dillio_Backend.API.ViewModel
 {
     public class ProductViewModel
     {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public float Discount { get; set; }
+        public string Description { get; set; }
+        public ICollection<Image> Images { get; set; }
+        public ICollection<Review> Reviews { get; set; }
     }
 }
