3961f7b37c7b9fb8e72505a7a602aeb453eabcec
Omar Ayyad <omarayyad36@gmail.com>
2019-06-25 13:01:39 +0200

    fixing stores paging

--- dillio-app/src/app/features/stores/store-listing/store-listing.component.ts
+++ dillio-app/src/app/features/stores/store-listing/store-listing.component.ts
@@ -3,6 +3,7 @@ import { Stores } from 'src/app/_models/stores';
 import { StoresService } from 'src/app/services/stores.service';
 import { Observable, throwError } from 'rxjs';
 import {MatPaginatorModule} from '@angular/material';
+import { PagingService } from 'src/app/services/paging.service';
 
 @Component({
   selector: 'app-store-listing',
@@ -12,11 +13,17 @@ import {MatPaginatorModule} from '@angular/material';
 export class StoreListingComponent implements OnInit {
 
   stores: Stores[];
+  allstores: Stores[];
   count:number[];
   errormessage:string;
   page:Number;
-
-  constructor(private storeService: StoresService) { 
+  CurrentPage:number;
+  totalpages:number;
+  pager: any = {};
+  Arr= Array;
+  constructor(
+    private storeService: StoresService,
+    private pagerService: PagingService,) { 
    this.count=[];
    this.page = 2;
   }
@@ -24,10 +31,36 @@ export class StoreListingComponent implements OnInit {
   ngOnInit() {
     this.storeService.getAll().subscribe(
       stores =>{
-        this.stores = stores;
+        this.allstores = stores;
+        this.CurrentPage = 1;
+        this.setPage(this.CurrentPage);
       },
       error =>this.errormessage = <any>error
     );
    
   }
+
+
+
+  setPage(page: number) {
+    // debugger;
+    this.pager = this.pagerService.getPage(this.allstores.length, page,18);
+
+    this.stores = this.allstores.slice(this.pager.startIndex, this.pager.endIndex + 1);
+
+    this.totalpages = this.pager.totalPages;
+  }
+
+
+  changePage(pageNum:number){
+    // debugger;
+    if(pageNum <= this.totalpages && pageNum >= 1){
+
+      this.CurrentPage = pageNum;
+      this.setPage(this.CurrentPage);
+      // window.scrollTo(0, 0);
+
+      window.scrollTo({ left: 0, top: 300, behavior: 'smooth' });
+    }
+}
 }
