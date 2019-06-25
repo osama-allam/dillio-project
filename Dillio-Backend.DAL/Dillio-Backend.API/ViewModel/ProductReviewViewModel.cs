dde6dad2d689e3bf8a959768f3bea77c334a9eb1
Ahmadaboelfath <ahmadaboelfath@gmail.com>
2019-06-25 23:37:29 +0200

    Pulling changes

---
+++ Dillio-Backend.DAL/Dillio-Backend.API/ViewModel/ProductReviewViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dillio_Backend.API.ViewModel
+{
+    public class ProductReviewViewModel
+    {
+
+        public int Id { get; set; }
+        public string ReviewDescription { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ProductId { get; set; }
+        public string UserId { get; set; }
+        public int StoreId { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public float Rating { get; set; }
+    }
+}
