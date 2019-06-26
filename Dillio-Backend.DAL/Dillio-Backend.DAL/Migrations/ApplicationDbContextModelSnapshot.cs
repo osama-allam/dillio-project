ecdd50259d5726d29e789516a5dd3a170d0e29ff
Osama <osama_allam@yamil.com>
2019-06-26 02:14:42 +0200

    finish review for product

--- Dillio-Backend.DAL/Dillio-Backend.DAL/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Dillio-Backend.DAL/Dillio-Backend.DAL/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,16 +4,14 @@ using Dillio_Backend.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dillio_Backend.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190625170439_InitialModel")]
-    partial class InitialModel
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
